8108f5bf59d5c131b6b317da29ee84bf668411ab
jiayun4921 <jia.yun4921@gmail.com>
2023-04-05 18:21:12 +0900

    04.05
    인스턴스

---
+++ JavaProject/SelfPractice/src/instanceSelf/SmartPhone01.java
@@ -0,0 +1,20 @@
+package instanceSelf;
+
+public class SmartPhone01 {
+
+	public static void main(String[] args) {
+
+		SmartPhone sp = new SmartPhone01();
+		
+		System.out.println("> color : " + sp.color);
+		System.out.println("> size : " + sp.size);
+		System.out.println("> volume : " + sp.volume);
+		
+		sp.call();
+		
+		
+		
+
+	}
+
+}
