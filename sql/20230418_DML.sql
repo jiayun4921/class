582e2e39f58ba6da46991c11fbbdbd7e5a702dfd
jiayun4921 <jia.yun4921@gmail.com>
2023-04-19 18:08:46 +0900

    04.18 DML

--- sql/20230418_DML.sql
+++ sql/20230418_DML.sql
@@ -171,5 +171,37 @@ select * from emp01;
 select deptno from dept02 where dname='SALES';
 delete from emp01 where deptno=(select deptno from dept02 where dname='SALES');
 
+commit;
+
+delete from emp;
+select * from emp;
+
+select * from emp01;
+delete from emp01;
+
+rollback;  ----롤백
+
+
+
+-- sequence : 연속된 숫자를 생성해주는 객체
+-- create sequence 이름 [옵션]
+
+
+drop sequence seq_dept_deptno;
+create sequence seq_dept_deptno
+start with 10
+increment by 10
+;
+
+-- 자동 생성된 값 : 시퀀스 객체이름.nextval
+select seq_dept_deptno.nextval
+from dual;
+select * from dept01;
+insert into dept01 values(seq_dept_deptno.nextval, 'test이름', 'test위치');
+
+select seq_dept_deptno.currval
+from dual;
+
+
  
  
