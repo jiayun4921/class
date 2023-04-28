cbf5fc8687e93ebf9451b7184ac0eb7a1253654a
jiayun4921 <jia.yun4921@gmail.com>
2023-04-28 18:18:08 +0900

    04.28 EL

---
+++ jspProject/FirstWeb/src/main/java/product/product.java
@@ -0,0 +1,16 @@
+package product;
+
+public class product {
+
+	private String[] list = {"item1","itme2","itme3","itme4"};
+	private int price=1000;
+	public String[] getList() {
+		return list;
+	}
+	public int getPrice() {
+		return price;
+	}
+	
+	
+	
+}
