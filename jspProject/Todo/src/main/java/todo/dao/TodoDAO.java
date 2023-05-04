fbb72d8e02ea2d52581f310317426e41563f88d2
jiayun4921 <jia.yun4921@gmail.com>
2023-05-04 16:17:21 +0900

    Todo

--- jspProject/Todo/src/main/java/todo/dao/TodoDAO.java
+++ jspProject/Todo/src/main/java/todo/dao/TodoDAO.java
@@ -25,6 +25,10 @@ public class TodoDAO {
 		return dao;
 	}
 	
+	
+	
+	
+	
 	// TodoDTO 리스트를 반환하는 메소드
 	public List<TodoDTO> selectByAll(Connection conn){
 		
@@ -48,13 +52,13 @@ public class TodoDAO {
 			
 			// List<TodoDTO> 에 저장 반환
 			while(rs.next()) {
-				int tno = rs.getInt("tno");
-				String todo = rs.getString("todo");
-				String duedate = rs.getString("duedate"); // 2023-05-05
-				boolean finished = rs.getBoolean("finished");
-				
-				TodoDTO dto = new TodoDTO(tno, todo, duedate, finished);
-				list.add(dto);
+//				int tno = rs.getInt("tno");
+//				String todo = rs.getString("todo");
+//				String duedate = rs.getString("duedate"); // 2023-05-05
+//				boolean finished = rs.getBoolean("finished");
+//				
+//				TodoDTO dto = new TodoDTO(tno, todo, duedate, finished);
+				list.add(makeTodoDTO(rs));
 			}
 			
 		} catch (SQLException e) {
@@ -78,6 +82,62 @@ public class TodoDAO {
 		return list;
 	}
 	
+	
+	// tno 값을 받아서 해당 Todo 정보(TodoDTO) 를 반환하는 메소드
+	public TodoDTO selectByTno(Connection conn, int tno) { // 2
+		
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		TodoDTO todo = null;
+		
+		// select Sql
+		String sql = "select * from tbl_todo where tno=?";
+		
+		try {
+			pstmt = conn.prepareStatement(sql);
+			// set
+			pstmt.setInt(1, tno);
+			
+			//rs
+			rs = pstmt.executeQuery();
+			
+			// if : 결과 행이 0 또는 1 일 때			
+			if(rs.next()) {
+				todo = makeTodoDTO(rs);
+			}
+			
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			
+			try {
+				if(rs!=null) {
+					rs.close();
+				}
+				if(pstmt!=null) {
+					pstmt.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			
+		}
+		
+		return todo;
+	}
+	
+	// rs -> TodoDTO 생성 => 반환
+	private TodoDTO makeTodoDTO(ResultSet rs) throws SQLException {
+		
+		return new TodoDTO(
+				rs.getInt("tno"), 
+				rs.getString("todo"), 
+				rs.getString("duedate"), 
+				rs.getBoolean("finished"));
+		
+	}
 	
 	
 	
@@ -116,6 +176,86 @@ public class TodoDAO {
 	}
 	
 	
+	// TodoDTO 전달 받고
+	// update 
+	public int updateByTno(Connection conn, TodoDTO todo) {
+		
+		PreparedStatement pstmt = null;
+		int result = 0;
+		
+		// update sql
+		String sql = "update tbl_todo set todo=?, duedate=?, finished=? where tno=?";
+		
+		try {
+			pstmt = conn.prepareStatement(sql);
+			// set
+			pstmt.setString(1, todo.getTodo());
+			pstmt.setString(2, todo.getDuedate());
+			pstmt.setBoolean(3, todo.isFinished());  // rs.getBoolean("finished") 0/1
+			pstmt.setInt(4, todo.getTno());
+			
+			// result
+			result = pstmt.executeUpdate();
+			
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			if(pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+		
+		
+		return result;
+		
+	}
+	
+	
+	// tno 전달받고
+	// delete
+	public int deleteByTno(Connection conn, int tno) {
+		
+		PreparedStatement pstmt = null;
+		int result = 0;
+		
+		// delete Sql
+		String sql = "delete from tbl_todo where tno=?";
+		
+		try {
+			pstmt = conn.prepareStatement(sql);
+			//set
+			pstmt.setInt(1, tno);
+			
+			// result
+			result = pstmt.executeUpdate();
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			
+			if(pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+		
+		return result;		
+	}
+	
+	
+	
 	
 	
 	
@@ -125,14 +265,33 @@ public class TodoDAO {
 		TodoDAO dao = TodoDAO.getInstance();
 		Connection conn = ConnectionProvider.getConnection();
 		
-		List<TodoDTO> list = dao.selectByAll(conn);
-		for(TodoDTO todo : list) {
-			System.out.println(todo);
-		}
+		// slectAll test
+//		List<TodoDTO> list = dao.selectByAll(conn);
+//		for(TodoDTO todo : list) {
+//			System.out.println(todo);
+//		}
+		
+		// insert test
+//		dao.insertTodo(conn, new RequestTodo("회의", "2023-05-03"));
+//		System.out.println("입력완료");
 		
 		
-		dao.insertTodo(conn, new RequestTodo("회의", "2023-05-02"));
+		// selectByTno test
+//		TodoDTO todo = dao.selectByTno(conn, 2);
+//		System.out.println(todo);
 		
+		
+		// update test
+//		dao.updateByTno(conn, new TodoDTO(2, "청소 후 휴식", "2023-05-03", true));
+//		System.out.println("수정완료...");
+		
+		// delete test
+		dao.deleteByTno(conn, 9);
+		System.out.println("삭제완료");
+		
+		
+		
+				
 		conn.close();
 		
 	}
--- jspProject/Todo/src/main/webapp/WEB-INF/views/todo/list.jsp
+++ jspProject/Todo/src/main/webapp/WEB-INF/views/todo/list.jsp
@@ -8,13 +8,13 @@
 <title>Insert title here</title>
 </head>
 <body>
-<h1>Todo LIST</h1>
+<h1>Todo LIST ( ${loginInfo.uid} [${loginInfo.uname}] ) </h1>
 
 <ul>
 
 	<c:forEach items="${todoList}" var="todo" >
 
-	<li><a href="${pageContext.request.contextPath}/todo/read?no=${todo.tno}">${todo.tno}. ${todo.todo} </a> ${todo.duedate} ${finished ? 'DONE' : 'NOT YET'} </li>
+	<li><a href="${pageContext.request.contextPath}/todo/read?no=${todo.tno}">${todo.tno}. ${todo.todo} </a> ${todo.duedate} ${todo.finished ? 'DONE' : 'NOT YET'} </li>
 	
 	</c:forEach>
 	
@@ -23,6 +23,17 @@
 
 
 <a href="${pageContext.request.contextPath}/todo/register">Todo Register</a>
+
+<c:if test="${loginInfo eq null}">
+<a href="/app/login"> 로그인 </a>
+</c:if>
+<c:if test="${loginInfo ne null}">
+<a href="/app/logout"> 로그아웃 </a>
+</c:if>
+
+
+
+
 
 </body>
 </html>
