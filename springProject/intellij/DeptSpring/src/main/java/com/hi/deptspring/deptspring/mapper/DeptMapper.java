c309313364aeab36f6e6f0ab766cab825629fbeb
jiayun4921 <jia.yun4921@gmail.com>
2023-05-16 18:21:07 +0900

    05.16 Spring myBatis

---
+++ springProject/intellij/DeptSpring/src/main/java/com/hi/deptspring/deptspring/mapper/DeptMapper.java
@@ -0,0 +1,27 @@
+package com.hi.deptspring.deptspring.mapper;
+
+import com.hi.deptspring.deptspring.domain.DeptDTO;
+import com.hi.deptspring.deptspring.domain.DeptRegistRequest;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface DeptMapper {
+
+    // 전체 부서 리스트
+    List<DeptDTO> selectAll();
+
+    // deptno 컬럼으로 검색 ==> DeptDTO
+    DeptDTO selectByDeptno(int deptno);
+
+    // DeptRegistRequest 를 받아서 insert해주는 메소드가 필요
+    int insertDept(DeptRegistRequest request);
+
+    // DeptDTO 전달받고 update 메소드
+    int updateDept(DeptDTO deptDTO);
+
+    int deleteDept(int deptno);
+
+
+}
